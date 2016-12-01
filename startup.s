6045b9d0b139e4ffd30b0112cce4788812006170
hujia.liuhj <hujia.liuhj@alibaba-inc.com>
2016-12-01 17:52:27 +0800

    Added comments for documents favour

--- startup.s
+++ startup.s
@@ -1,5 +1,87 @@
 .code 32
 
+/*
+=======================================================================================
+Usr         System         Supervisor      Abort         Undefined         IRQ           FIQ
+----------------------------------------------------------------------------------------------------------------
+R0          R0             R0              R0            R0                R0            R0
+R1          R1             R1              R1            R1                R1            R1
+R2          R2             R2              R2            R2                R2            R2         
+R3          R3             R3              R3            R3                R3            R3
+R4          R4             R4              R4            R4                R4            R4
+R5          R5             R5              R5            R5                R5            R5
+R6          R6             R6              R6            R6                R6            R6
+R7          R7             R7              R7            R7                R7            R7
+---------------------------------------------------------------------------------------------------------------
+R8          R8             R8              R8            R8                R8            R8_fiq
+R9/sb       R9             R9              R9            R9                R9            R9_fiq
+R10/sl      R10            R10             R10           R10               R10           R10_fiq
+R11/fp      R11            R11             R11           R11               R11           R11_fiq
+R12/ip      R12            R12             R12           R12               R12           R12_fiq
+R13/sp      R13            R13_svc         R13_abt       R13_und           R13_irq       R13_fiq
+R14/lr      R14            R14_svc         R14_abt       R14_und           R14_irq       R14_fiq
+PC          PC             PC              PC            PC                PC            PC
+CPSR        CPSR           CPSR            CPSR          CPSR              CPSR          CPSR
+                           SPSR_svc        SPSR_abt      SPSR_und          SPSR_irq      SPSR_fiq
+=======================================================================================
+R0-R7: all modes the same
+R8-R14: diff mode in diff physical register, fiq will be faster
+R14/lr: each interrupt mode takes its own R14 to cache return address(+4)
+R14_irq: when IRQ happens, R14 will remain unchanged from usr/system mode,
+         R14_irq saves usr/system address(+4), once enable irq when irq happens,
+         R14_irq will be covered.
+
+R15/PC:
+-------------------------------------------
+| address   |    code     | state         |
+| PC-8      | LDR R0, PC  | Running       |
+| PC-4      | ???         | Translating   |
+| PC        | ???         | Picking       |
+-------------------------------------------
+
+*/
+
+/*
+======== R0-R4 Register Usage ===================
+** Function Call <= 4 ************************************
+str lr, [sp, #-4]! // push current lr to sp pointed stack, let sp-=4
+ldr r0, =0x01      // param1
+ldr r1, =0x02      // param2
+ldr r2, =0x03      // param3
+ldr r3, =0x04      // param4
+bl func_c          // jump function
+ldr lr, [sp], #4   // pop from sp pointed stack
+
+** Function Call > 4 ************************************
+str lr, [sp, #-4]!
+ldr r0, =0x01      // param1
+ldr r1, =0x02      // param2
+ldr r2, =0x03      // param3
+ldr r3, =0x04      // param4
+
+ldr r4, =0x08      // param8
+str r4, [sp, #-4]! // push param8 into sp pointed stack, let sp-=4
+ldr r4, =0x07      // param7
+str r4, [sp, #-4]!
+ldr r4, =0x06      // param6
+str r4, [sp, #-4]!
+ldr r4, =0x05      // param5
+str r4, [sp, #-4]!
+
+bl func_c          // jump function
+
+add sp, sp, #4     // let sp+=4
+add sp, sp, #4     // let sp+=4
+add sp, sp, #4     // let sp+=4
+add sp, sp, #4     // let sp+=4
+
+ldr lr, [sp], #4   // pop from sp pointed stack
+=================================================
+
+
+
+*/
+
 /* _start entry
  * vector interrupt table:
  * 0x00: ldr pc, _reset
@@ -42,10 +124,9 @@ _start:
  * s[statement] psr[16-23]
  * f[flag]      psr[24-31]
  *
- * The Status register:
+ * ================= The Status register ============================
  * cpsr[current processor status register]
  * spsr[saved processor status register]
-
  * |31 30 29 28    7  6  5   4  3  2  1  0|
  * | N  Z  C  V....I  F  T  M4 M3 M2 M1 M0|
  * [N] save as calc-res bit[31],1(>=0),0(<0)
