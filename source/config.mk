02ea86b6a128ae5e244c1170bdd1f333cf39ff28
hujia.liuhj <hujia.liuhj@alibaba-inc.com>
2017-03-14 12:13:40 +0800

    Update makefile mechanism&essence

--- source/config.mk
+++ source/config.mk
@@ -1,6 +1,32 @@
+# Prefix platform
 PLATFORM    = arm
 BOARD       = qemu
 CPU         = arm926ej-s
 
+BUILD       = build
+
+# Makefile main logic
+ROOT=$(shell pwd)
+PRE_ROOT=$(ROOT)/../
+CC    =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/bin/$(PLATFORM)-none-eabi-gcc
+AS    =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/bin/$(PLATFORM)-none-eabi-as
+LD    =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/bin/$(PLATFORM)-none-eabi-ld
+OC    =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/bin/$(PLATFORM)-none-eabi-objcopy
+LDIR  =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/$(PLATFORM)-none-eabi/lib
+LDIR2 =$(ROOT)/../$(BUILD)/toolchain/$(PLATFORM)/lib/gcc/$(PLATFORM)-none-eabi/4.9.3
+
+OUT_DIR=../out/$(PLATFORM)
+OUT_BIN = output
+
+
 C_CFLAGS += -Iarch/$(PLATFORM)/$(BOARD)/include
 C_CFLAGS += -I./
+
+AFLAGS=-mcpu=$(CPU)
+CFLAGS=-mcpu=$(CPU)  \
+       -nostdlib -g \
+       $(C_CFLAGS)
+
+LDFLAGS = -L$(LDIR) \
+          -L$(LDIR2) \
+	  -lc -lgcc -lm 
