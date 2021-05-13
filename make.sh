49e69b1bc890f4d7c72a5f8c4df7c07d9e154518
steven.liu <steven.liu@paytm.com>
2021-05-13 16:32:03 -0400

    Refined make system

--- make.sh
+++ make.sh
@@ -16,32 +16,9 @@
 ##
 ####
 
-BUILD=build
-TARGET=arm
-PLATFORM=./$BUILD/toolchain/$TARGET
-
-if [ -d "$PLATFORM" ];then
+cp ./configuration ./source/config.mk
+cp ./build/Makefile ./source
 echo ""
-echo [1]Got corresponding platform:[$TARGET] in $PLATFORM
-else
-echo ""
-echo ""
-echo [1]No platform, unzip default one.
-echo Please wait...
-sleep 1
-tar jxvf ./$BUILD/$TARGET.tar.bz2
-mv toolchain/* $BUILD/toolchain/
-fi
-
-cp $BUILD/Makefile ./source
-echo ""
-echo [2]Start make all...
+echo [Start Compiling]
 cd source
 make all
-#./bin/w500/bin/arm-none-eabi-as -mcpu=arm926ej-s startup.s -o startup.o
-#./bin/w500/bin/arm-none-eabi-as -mcpu=arm926ej-s iv.s -o iv.o
-#./bin/w500/bin/arm-none-eabi-gcc -c -mcpu=arm926ej-s init.c -o init.o 
-#./bin/w500/bin/arm-none-eabi-gcc -c -mcpu=arm926ej-s timer.c -o timer.o 
-#./bin/w500/bin/arm-none-eabi-ld -T linker.ld  init.o  timer.o -o output.elf 
-#./bin/w500/bin/arm-none-eabi-objcopy -O binary output.elf output.bin
-##qemu-system-arm -M versatilepb -nographic -kernel output.bin
